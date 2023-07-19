90da34e68710b6204bda40f7b3370b1281aa49fc
ImDaeseong <cs93059@gmail.com>
2023-07-19 18:52:04 +0900

    AndroidJetpack

--- Row_test/app/src/main/java/com/daeseong/row_test/Row1Activity.kt
+++ Row_test/app/src/main/java/com/daeseong/row_test/Row1Activity.kt
@@ -3,38 +3,154 @@ package com.daeseong.row_test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.daeseong.row_test.ui.theme.Row_testTheme
 
 class Row1Activity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row_testTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Test1()
-                }
-            }
+            Test1()
         }
     }
 }
 
 @Composable
 fun Test1() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray))
+    {
+        Row(
+            modifier = Modifier
+                .background(Color.White)
+                .border(BorderStroke(1.dp, Color.Red))
+                .height(50.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center) {
+
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .wrapContentSize(Alignment.Center),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                text = "Row1"
+            )
+
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .wrapContentSize(Alignment.Center),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                text = "Row2"
+            )
+
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .wrapContentSize(Alignment.Center),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                text = "Row3"
+            )
+
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .wrapContentSize(Alignment.Center),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                text = "Row4"
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .background(Color.White)
+                .border(BorderStroke(1.dp, Color.Red))
+                .height(50.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center) {
+
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .wrapContentSize(Alignment.Center),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                text = "Row1"
+            )
+
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .wrapContentSize(Alignment.Center),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                text = "Row2"
+            )
+
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .wrapContentSize(Alignment.Center),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                text = "Row3"
+            )
+
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .wrapContentSize(Alignment.Center),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                text = "Row4"
+            )
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview2() {
-    Row_testTheme {
-        Test1()
-    }
+    Test1()
 }
